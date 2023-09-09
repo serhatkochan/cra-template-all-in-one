8d5f2192f39348a6c811c13db50b6b276923a5bc
Serhat <serhatkochan@hotmail.com.tr>
2023-09-09 19:49:37 +0300

    *

--- template/src/store/redux/reducers/System.js
+++ template/src/store/redux/reducers/System.js
@@ -1,12 +1,11 @@
 import SystemHelper from 'helpers/SystemHelper.js';
-import { SET_LANGUAGE } from 'store/redux/action-types/System'
+import { SET_LANGUAGE } from 'store/redux/action-types/System';
 
 const { GetLocalSystemConfig } = SystemHelper;
 
 const initialState = GetLocalSystemConfig();
 
 export default function System(state = initialState, action) {
-
   const { type, payload } = action;
   switch (type) {
     case SET_LANGUAGE:
